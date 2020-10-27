ec10997da1e46675247d4a3abf30854d624232da
AlekseyMikhailovski <aleksmihai00@gmail.com>
2020-10-27 17:47:10 +0300

    Trying to do tasks 9-11

--- src/HomeWork4/DataContainer.java
+++ src/HomeWork4/DataContainer.java
@@ -2,15 +2,27 @@ package HomeWork4;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
-public class DataContainer<T> {
-    private T[] data = (T[])new Object[1];
+public class DataContainer<T> implements Iterable<T>, Comparable<T>{
+    private T[] data ;
+
+    public DataContainer(){
+        this.data =  (T[]) new Object[0];
+    }
+    public DataContainer(T[] data) {
+        this.data = data;
+    }
 
     public int add(T item){
+        if ( item == null )
+            return -1;
         int i = 0;
         int pos = -1;
         while (i < data.length){
-            if (data[i] == null){
+            if ( data[i] == null ){
                 data[i] = item;
                 pos = i;
                 break;
@@ -26,7 +38,7 @@ public class DataContainer<T> {
     }
 
     public T get(int index){
-        if (index >= data.length)
+        if ((index >= data.length) || (index < 0))
             return null;
         return data[index];
     }
@@ -51,13 +63,9 @@ public class DataContainer<T> {
     }
 
     public boolean delete(T item){
-        if (item == data[data.length-1]){
-            data = Arrays.copyOf(data,data.length-1);
-            return true;
-        }
         int i = 0;
         while (i < data.length){
-            if (data[i] == item ){
+            if ( data[i] != null && data[i].equals(item) ){
                 for (int j = i; j < data.length - 1 ; j++) {
                     data[j] = data[j+1];
                 }
@@ -69,16 +77,18 @@ public class DataContainer<T> {
         return false;
     }
 
-    public void sort(Comparator<T> comparator){ //change to fast sort and write DataComparator and foreach etarable
+    public void sort(Comparator<T> comparator){
         int f = 0;
         T buff;
         for (int j = 0; j < data.length; j++) {
             for (int i = 0; i < data.length; i++) {
-                f = comparator.compare(data[i], data[i+1]);
-                if (f > 0){
-                    buff = data[i];
-                    data[i] = data[i+1];
-                    data[i+1] = buff;
+                if (i != data.length - 1){
+                    f = comparator.compare(data[i], data[i+1]);
+                    if (f >= 0){
+                        buff = data[i];
+                        data[i] = data[i+1];
+                        data[i+1] = buff;
+                    }
                 }
             }
         }
@@ -96,13 +106,56 @@ public class DataContainer<T> {
                 '}';
     }
 
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return null;
+    }
+
+    public void set(T value,int index){
+        this.data[index] = value;
+    }
 //    public static void sort(DataContainer<? extends Comparable> container){
-//        Comparator<?> comparator ;
-//        comparator.compare(container.data[1],container.data[2]);
-//
+//        int f = 0;
+//        Object buff,buff2;
+//        for (int j = 0; j < container.data.length; j++) {
+//            for (int i = 0; i < container.data.length-1; i++) {
+//                f = container.data[i].compareTo(container.data[i+1]);
+//                if (f >= 0){
+//                    buff = container.get(i);
+//                    buff2 = container.get(i+1);
+//                    container.set(((? extends Comparable) buff2),i);
+//                }
+//            }
+//        }
 //    }
+
+//    public static void sort(DataContainer<?> container, Comparator<?> comparator){
+//        int f = 0;
+//        Object buff,buff2;
+//        for (int j = 0; j < container.data.length; j++) {
+//            for (int i = 0; i < container.data.length; i++) {
+//                f = comparator.compare(container.get(i), container.get(i+1));
+//                if (f >= 0){
+//                    buff = container.get(i);
+//                    buff2 = container.get(i+1);
 //
-//    public static void sort(DataContainer<? extends Comparable> container, Comparator<?> comparator){
-//
+//                }
+//            }
+//        }
 //    }
+
+    @Override
+    public int compareTo(T o) {
+        return this.toString().compareTo(o.toString());
+    }
 }
--- src/HomeWork4/DataContainerMain.java
+++ src/HomeWork4/DataContainerMain.java
@@ -4,29 +4,19 @@ import java.util.Comparator;
 
 public class DataContainerMain {
     public static void main(String[] args) {
-        DataContainer<Integer> data = new DataContainer<>();
-        //Comparator<Integer> comparator;
+        Integer[] numbers = new Integer[5];
+        DataContainer<Integer> data = new DataContainer<Integer>(numbers);
+        DataComparator<Integer> comparator = new DataComparator<>();
 
-        for (int i = 1; i < 11; i++) {
-            data.add(i);
-        }
-        System.out.println(data.get(9));
-        System.out.println(data.add(345));
-        data.add(null);
-        System.out.println(data.add(347));
-        System.out.println(data.delete(0));
-        System.out.println(data.delete((Integer) 347));
-        System.out.println(data.delete(347));
-
-        data.add(null);
-        data.add(null);
-        data.add(null);
-        data.add(null);
-        data.add(2);
-        data.add(5);
-        data.add(123);
-        data.add(7);
+        System.out.println(data.add(1));
+        System.out.println(data.add(999));
+        System.out.println(data.add(50));
+        System.out.println(data.add(100));
+        System.out.println(data.add(100));
+        System.out.println(data.add(10));
         System.out.println(data.toString());
-       // data.sort(comparator);
+
+        data.sort(comparator);
+        System.out.println(data.toString());
     }
 }
